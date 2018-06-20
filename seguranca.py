39a3eecbddca5db515d203155e23255b83979b24
leonardork <leodegolim@yahoo.com.br>
2018-06-20 16:18:54 -0300

    arrumando identação código

--- seguranca.py
+++ seguranca.py
@@ -10,7 +10,7 @@ def error_log(message):
 
 if __name__ == "__main__":
     HOST, PORT = "0.0.0.0", 7000
-    MAX_TIME =  10 # 60 segundos vezes 10 = 10 minutos
+    MAX_TIME =  60 * 10 # 60 segundos vezes 10 = 10 minutos
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind((HOST, PORT))
     sock.listen(10)
@@ -24,7 +24,19 @@ if __name__ == "__main__":
     
     while True:
         conn, addr = sock.accept()
-        msg = conn.recv(1024)
+        sock.setblocking(False)
+
+        try:
+            msg = conn.recv(1024)
+        except IOError as e:  # and here it is handeled
+            if e.errno == errno.EWOULDBLOCK:
+                print("passou no if")
+                pass
+            else:
+                print("passou no else")
+                pass
+        
+
         if(msg):
             if "TEMPERATURE" in str(msg):
                 last_pulse_temperature = time.time()
@@ -32,32 +44,32 @@ if __name__ == "__main__":
                 last_pulse_pressure = time.time()
             if "GAS" in str(msg):
                 last_pulse_gas = time.time()
-            if "SERVER" in str(msg):				
+            if "SERVER" in str(msg):                
                 last_pulse_server = time.time()
             if "WATER" in str(msg):
                 last_pulse_water = time.time()
 
-    past_pulse_temperature = time.time() - last_pulse_temperature
-    past_pulse_pressure = time.time() - last_pulse_pressure
-    past_pulse_gas = time.time() - last_pulse_gas
-    past_pulse_server = time.time() - last_pulse_server
-    past_pulse_water = time.time() - last_pulse_water
-    
-    print("-----------------------------")	
-    print("Temperatura: " + str(past_pulse_temperature))
-    print("Pressão: " + str(past_pulse_pressure))
-    print("Nível: " + str(past_pulse_gas))
-    print("Servidor: " + str(past_pulse_server))
-    print("Água: " + str(past_pulse_water))
-    print("-----------------------------")
-    
-    if(past_pulse_temperature >= MAX_TIME):
-        error_log('RESISTENCE')
-    if(past_pulse_pressure >= MAX_TIME):
-        error_log('PRESSURE')
-    if(past_pulse_gas >= MAX_TIME):
-        error_log('GAS')
-    if(past_pulse_server >= MAX_TIME):
-        error_log('SERVER')
-    if(past_pulse_water >= MAX_TIME):
-        error_log('WATER')
+        past_pulse_temperature = time.time() - last_pulse_temperature
+        past_pulse_pressure = time.time() - last_pulse_pressure
+        past_pulse_gas = time.time() - last_pulse_gas
+        past_pulse_server = time.time() - last_pulse_server
+        past_pulse_water = time.time() - last_pulse_water
+        
+        print("-----------------------------")  
+        print("Temperatura: " + str(past_pulse_temperature))
+        print("Pressão: " + str(past_pulse_pressure))
+        print("Nível: " + str(past_pulse_gas))
+        print("Servidor: " + str(past_pulse_server))
+        print("Água: " + str(past_pulse_water))
+        print("-----------------------------")
+        
+        if(past_pulse_temperature >= MAX_TIME):
+            error_log('RESISTENCE')
+        if(past_pulse_pressure >= MAX_TIME):
+            error_log('PRESSURE')
+        if(past_pulse_gas >= MAX_TIME):
+            error_log('GAS')
+        if(past_pulse_server >= MAX_TIME):
+            error_log('SERVER')
+        if(past_pulse_water >= MAX_TIME):
+            error_log('WATER')
